a791da38ebf7089538e9617cd88bab690625ae2e
zhumagulmur95 <zhumagul.mur@gmail.com>
2022-09-08 18:19:20 +0600

    add Procfile3

--- tourist/settings.py
+++ tourist/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-i9&_rlckgabn^@3q^fos^-@wu-)i)z-0@_0hv7+w4iusnghvq@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
