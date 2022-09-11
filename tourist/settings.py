ef0bec01c7ceaa082019ba04dfbb258169f66b88
zhumagulmur95 <zhumagul.mur@gmail.com>
2022-09-11 15:50:30 +0600

    First commit!

--- tourist/settings.py
+++ tourist/settings.py
@@ -67,6 +67,19 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
+    'django.middleware.common.CommonMiddleware',
+]
+
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+
+CORS_ALLOWED_ORIGINS = [
+    'https://touristbish.herokuapp.com/',
+]
+
+CORS_ALLOWED_ORIGIN_REGEXES = [
+    'https://touristbish.herokuapp.com/',
 ]
 
 ROOT_URLCONF = 'tourist.urls'
